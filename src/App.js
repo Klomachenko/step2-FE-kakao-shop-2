3218e04566603f8a8dd3500768760690647d134c
Klomachenko <leekyumin0901@naver.com>
2023-08-04 02:48:09 +0900

    feat: 배포환경 설정 변경

--- src/App.js
+++ src/App.js
@@ -6,22 +6,24 @@ import MainLayout from "./layouts/MainLayout";
 import ProductDetailPage from "./pages/ProductDetailPage";
 import CartPage from "./pages/CartPage";
 
+const staticServerUri = process.env.REACT_APP_PATH || "http://kakao-app-env.eba-kfsgeb74.ap-northeast-2.elasticbeanstalk.com"
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />}></Route>
-          <Route path="/signup" element={<RegisterPage />}></Route>
+          <Route path={staticServerUri + "/login"} element={<LoginPage />}></Route>
+          <Route path={staticServerUri + "/signup"} element={<RegisterPage />}></Route>
           {/* {공통 레이아웃} */}
           <Route element={<MainLayout />}>
-            <Route path="/" element={<MainPage />}></Route>
-            <Route path="/" element={<ProductDetailPage />}></Route>
+            <Route path={staticServerUri + "/"} element={<MainPage />}></Route>
+            <Route path={staticServerUri + "/"} element={<ProductDetailPage />}></Route>
           </Route>
         </Routes>
         <Route element={<RequiredAuthLayout />}>
-          <Route path="/cart" element={<CartPage />}></Route>
-          <Route path="/order" element={<OrderPage />}></Route>
+          <Route path={staticServerUri + "/cart"} element={<CartPage />}></Route>
+          <Route path={staticServerUri + "/order"} element={<OrderPage />}></Route>
         </Route>
       </BrowserRouter>
     </div>
